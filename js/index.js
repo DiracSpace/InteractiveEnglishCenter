15119783a7c725de7aaef51f74f0901d8e52c5e1
Roberto De León <deleon.martinez.16138@itsmante.edu.mx>
2020-02-15 09:09:49 -0600

    added auth

--- js/index.js
+++ js/index.js
@@ -1,18 +1,19 @@
 /* DOM manipulation js */
 // load materialize
 document.addEventListener('DOMContentLoaded', function () {
-    M.AutoInit();
+  M.AutoInit();
 });
 
-/* pause/play video on mouse hover 
-*
-$(document).ready(function() {
-    $(".myvideos").on("mouseover", function(event) {
-      this.pause();
-  
-    }).on('mouseout', function(event) {
-      this.play();
-  
-    });
-});
-*/
+(function (d, m) {
+  var kommunicateSettings = { "appId": "75026ecb075b74a8285bc7e78a5703a5", "popupWidget": true, "automaticChatOpenOnNavigation": true };
+  var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+  s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+  var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+  window.kommunicate = m; m._globals = kommunicateSettings;
+})(document, window.kommunicate || {});
+
+function initMap() {
+  var schoolCoord = { lat: 22.744045, lng: -98.977668 };
+  var map = new google.maps.Map(document.getElementById('map'), { zoom: 19, center: schoolCoord });
+  var marker = new google.maps.Marker({ position: schoolCoord, map: map });
+}
